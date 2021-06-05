c1a57da6cb4ceed0ae429d6b50461790b496a05a
Gayan <gayanvirajith@gmail.com>
2021-06-05 16:31:26 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,7 @@ const wrapper = shallow(<App />);
 
 describe("<App />", () => {
   test("renders learn react link", () => {
+    // hello
     expect(wrapper.find(".App").text()).toBe("Hello world");
     console.log(wrapper.debug());
   });
