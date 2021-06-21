2db7c0a98bbf323745e4f24097b0210c1dda01e4
Gayan <gayanvirajith@gmail.com>
2021-06-21 10:31:04 +0530

    datepicker icon css fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+let a = '1'
+let b
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <div className="app-header">Hello world</div>
       <button>Click</button>
     </div>
   )
